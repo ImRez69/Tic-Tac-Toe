5a327198dcf172fdd80a5fdab4aec69fd8cc62aa
ImRez69 <CaptianAlirez@gmail.com>
2025-05-30 19:43:52 +0330

    Update

--- script.js
+++ script.js
@@ -9,34 +9,55 @@ const onClick = () => {
 };
 button.addEventListener("click", onClick);
 
+// Tic Tac Toe Rule
+let currentPlayer;
+
+const winningConditions = [
+  [0, 1, 2], // ردیف اول
+  [3, 4, 5], // ردیف دوم
+  [6, 7, 8], // ردیف سوم
+  [0, 3, 6], // ستون اول
+  [1, 4, 7], // ستون دوم
+  [2, 5, 8], // ستون سوم
+  [0, 4, 8], // قطر اصلی
+  [2, 4, 6], // قطر فرعی
+];
+
+let board = [
+  ["", "", ""], // ردیف اول
+  ["", "", ""], // ردیف دوم
+  ["", "", ""], // ردیف سوم
+];
+
+let turnCount = 0;
+
 // Box Action
 const article = document.getElementById("article");
-let currentPlayer = "X";
 
 const onClickBox = (e) => {
   const boxCheck = e.target.closest(".box"); // If .box is exited True else is False
 
   if (boxCheck) {
-    if (currentPlayer === "X") {
+    if (turnCount % 2 == 0) {
       // Player Turn
-      currentPlayer = "O";
-    } else {
+      turnCount++;
       currentPlayer = "X";
+    } else {
+      currentPlayer = "O";
+      turnCount++;
     }
 
-    switch (true) {
-      case currentPlayer === "X":
+    switch (currentPlayer) {
+      case "X":
         boxCheck.querySelector(".x-svg").classList.add("show");
         break;
 
-      case currentPlayer === "O":
+      case "O":
         boxCheck.querySelector(".o-svg").classList.add("show");
         break;
     }
-
     boxCheck.classList = "box-active";
   }
-
 
 };
 article.addEventListener("click", onClickBox);
